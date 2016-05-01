51a10d02d6e73997e8840d174b7985921ac99029
Linus <llrastegar@users.noreply.github.com>
2016-05-01 12:43:50 -0500

    The DRY Dude is Still Yelling at Me
    At least it works.

--- mathparser.py
+++ mathparser.py
@@ -1,19 +1,25 @@
 import re
 def m_eval(num, op, nun):
+	numer = int(num)
+	nuner = int(nun)
 	if any(char.isdigit() for char in op):
 		m_eval(num, "+", int(op))
-	elif op=="*": return int(num)*int(nun)
-	elif op=="+": return int(num)+int(nun)
-	elif op=="/": return int(num)/int(nun)
-	elif op=="^": return int(num)**int(nun)
-test = "2-5*2-17^2+3^2+2"
-operators = re.findall("\+|\*|\^|\/|-[0-9]+|[0-9]+", test)
+	elif op=="*": return numer*nuner
+	elif op=="+": return numer+nuner
+	elif op=="/": return numer/nuner
+	elif op=="^": return numer**nuner
+	elif op=="-": return numer - nuner
+test = "2-5*2--17^3-3^2"
+operators = re.findall("\+|\*|\^|\/|-|-[0-9]+|[0-9]+", test)
 counter=3
 if len(operators)>1:
 	if "-" in test:
-		for e in range(0, len(operators)):
+		for e in range(0, len(operators)-2):
 			if any(char.isdigit() for char in operators[e]) and any(char.isdigit() for char in operators[e+1]):
 				operators.insert(e+1, "+")
+			if operators[e]=="-" and operators[e+1]=="-":
+				operators[e+2] = "-" + operators[e+2]
+				operators.pop(e+1)
 	print operators
 	b = 0
 	while True:
@@ -39,6 +45,7 @@ if len(operators)>1:
 		a = m_eval(a, operators[counter], operators[counter+1])
 		counter+=2
 	print a
+	#fix subtraction in exponents
 	#add parentheses
 else:
 	print operators[0]
