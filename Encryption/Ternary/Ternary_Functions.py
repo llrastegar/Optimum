d57f86d4765e3eb0a33312ab5ea98d3aa66db789
Linus <llrastegar@users.noreply.github.com>
2016-05-19 17:43:51 -0500

    Corrected Ternary Functions

--- Encryption/Ternary/Ternary_Functions.py
+++ Encryption/Ternary/Ternary_Functions.py
@@ -5,13 +5,11 @@
 # Will later be expanded to Haskell
 #-----------------------
 def t_and(t=[]):
-	print t
 	if 0 in t: return 0
-	elif 1 in t: return 1
-	else: return 2
+	elif 2 in t: return 2
+	else: return 1
 	
 def t_or(t=[]):
-	print t
 	if 1 in t: return 1
 	elif 0 in t: return 0
 	else: return 2
