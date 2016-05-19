3699af52c5e7c91833171333a81eecaa25dbfe25
Linus <llrastegar@users.noreply.github.com>
2016-05-19 18:45:52 -0500

    Added XOR function

--- Encryption/Ternary/Ternary_Functions.py
+++ Encryption/Ternary/Ternary_Functions.py
@@ -13,5 +13,14 @@ def t_or(t=[]):
 	if 1 in t: return 1
 	elif 0 in t: return 0
 	else: return 2
+def t_xor(t=[]):
+	if 0 not in t and 2 not in t:
+		return 0
+	elif 1 in t and 2 in t:
+		return 2
+	elif 0 in t and 1 in t:
+		return 1
+	else:
+		return 2
 	
-print t_and([1,2,2])
+print t_and([1,2])
