2913079a87cb535ea43d802e607ba32f2a8760f7
Alex Bohm <aabohm7@gmail.com>
2016-05-20 19:29:04 -0500

    Added ternary to int function and NOT

--- Encryption/Ternary/Ternary_Functions.py
+++ Encryption/Ternary/Ternary_Functions.py
@@ -4,6 +4,13 @@
 # Will include ternary not, or, and, xor, and inverse functions
 # Will later be expanded to Haskell
 #-----------------------
+def to_int(st=""):
+	num = 0
+	p = 0
+	for tit in st[::-1]:
+		num += int(tit)*3**p
+		p += 1
+	return num
 def t_and(t=[]):
 	if 0 in t: return 0
 	elif 2 in t: return 2
@@ -26,4 +33,5 @@ def t_not(t=2):
 	if t==0: return 1
 	elif t==2: return 2
 	else: return 0
-print t_and([1,2])
+
+print to_int("201") #should be 19 and is
